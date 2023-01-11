1673469d1a5efc774d6db39b5369dc17de78932a
Gerson Sobrinho <sobrinho.gerson@gmail.com>
2023-01-11 16:44:54 -0300

    Initial commit

---
+++ src/main/kotlin/Exercicio.kt
@@ -0,0 +1,15 @@
+//import java.awt.desktop.PrintFilesEvent
+
+fun main() {
+
+    println("A função retorna: "); helloWorlf()
+    println("A Soma de 10 + 9 é: ${sum(10,9)}")
+
+}
+fun sum (a: Int, b: Int): Int {
+    return (a + b)
+}
+fun helloWorlf(){
+    println("Hello World!")
+    println("Programa do Gerson!")
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    println("Hello World!")
+    println("Meu primeiro programa em Kotlin!")
+    println(3*4+7)
+    println("Double Max ${Double.MAX_VALUE} - MIN ${Double.MIN_VALUE}")
+
+}
